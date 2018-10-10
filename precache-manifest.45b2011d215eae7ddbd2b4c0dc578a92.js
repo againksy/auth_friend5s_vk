2ade87559a080c1f85ff4c2da953c370aa6a1692
z <z@zs-MacBook-Air.local>
2018-10-10 15:40:39 +0300

    Updates

---
+++ precache-manifest.45b2011d215eae7ddbd2b4c0dc578a92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/auth_friend5s_vk/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f47c71b33390cd48438e",
+    "url": "/auth_friend5s_vk/static/js/runtime~main.f47c71b3.js"
+  },
+  {
+    "revision": "d0aa635dc8a9d49f2c9e",
+    "url": "/auth_friend5s_vk/static/js/main.d0aa635d.chunk.js"
+  },
+  {
+    "revision": "96216258dc72830184be",
+    "url": "/auth_friend5s_vk/static/js/1.96216258.chunk.js"
+  },
+  {
+    "revision": "d0aa635dc8a9d49f2c9e",
+    "url": "/auth_friend5s_vk/static/css/main.9bdbb526.chunk.css"
+  },
+  {
+    "revision": "f494a0499ac977333f8419862a147bf1",
+    "url": "/auth_friend5s_vk/index.html"
+  }
+];
